e2ad05a61fde862e09debc46cceee90dac1346b0
jagadeesh <addalacheruvujagadeesh@gmail.com>
2024-08-15 15:39:02 +0530

    Git initial push

---
+++ src/fitness.js
@@ -0,0 +1,15 @@
+const_dirname = path.dirname("")
+const buildPath = path.join(_dirname, "../build");
+
+app.use(express.static(buildPath))
+
+app.get("/*", function(req, res) {
+    res.sendFile(
+        path.join(_dirname,"../client/build/index.html"),
+        function (err){
+            if(err){
+                res.status(500).send(err);
+            }
+        }
+    );
+})
